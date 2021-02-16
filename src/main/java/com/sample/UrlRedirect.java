98144b9d71cfe1fddbed4c43cab9fd07460c857e
Pramodh M <54981492+pramodhm112@users.noreply.github.com>
2021-02-16 10:34:17 +0530

    Update UrlRedirect.java

--- src/main/java/com/sample/UrlRedirect.java
+++ src/main/java/com/sample/UrlRedirect.java
@@ -12,6 +12,9 @@ public class UrlRedirect extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // BAD: a request parameter is incorporated without validation into a URL redirect
-        response.sendRedirect(request.getParameter("target"));
+        // response.sendRedirect(request.getParameter("target"));
+        
+        if (VALID_REDIRECT.equals(request.getParameter("target"))) {
+			response.sendRedirect(VALID_REDIRECT);
     }
 }
